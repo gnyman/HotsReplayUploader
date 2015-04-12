4a7e6db833f2f6900e4b3c6c410b7eb5bc5f1562
Gabriel N <gabriel@walkbase.com>
2015-04-12 19:23:27 +0200

    s3 credential template file

---
+++ HotsReplayUploader/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  HotsReplayUploader
+//
+//  Created by Gabriel N on 12/04/15.
+//  Copyright (c) 2015 Gabriel N. All rights reserved.
+//
+//  This file needs to contain the amazon s3 details
+//  I'm leaving them out from the github project
+//  if you are interested in improving this project
+//  please contact either me or the Hotslog.com maintainer
+//  to get the needed details.
+
+let s3secret = "S3SECRET"
+let bucket = "BUCKET"
+let s3accessKey = "ACCESSKEY"
