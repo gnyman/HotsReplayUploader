eeff28e1c4fc72db366bb35cb493ba2d34da451e
Gabriel N <gabriel@walkbase.com>
2015-04-12 19:19:00 +0200

    Initial commit
    This is a big commit as the previous project stopped working in Xcode, so most of this code comes from a previous project. But in the end, this is for the better as this allowed me to do a desperately needed refactoring.

---
+++ HotsReplayUploader/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  HotsReplayUploader
+//
+//  Created by Gabriel N on 12/04/15.
+//  Copyright (c) 2015 Gabriel N. All rights reserved.
+//
+
+#import <CommonCrypto/CommonHMAC.h>
---
+++ HotsReplayUploader/HMACAlgorithm.swift
@@ -0,0 +1,9 @@
+//
+//  HMACAlgorithm.swift
+//  HotsReplayUploader
+//
+//  Created by Gabriel N on 12/04/15.
+//  Copyright (c) 2015 Gabriel N. All rights reserved.
+//
+
+import Foundation
