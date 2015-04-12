eddaa2239b0ee903c1fcf3a0254ce204e20732d7
Gabriel N <gabriel@walkbase.com>
2015-04-12 10:00:19 +0200

    Initial Commit

---
+++ HotsReplayUploader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HotsReplayUploader
+//
+//  Created by Gabriel N on 12/04/15.
+//  Copyright (c) 2015 Gabriel N. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
